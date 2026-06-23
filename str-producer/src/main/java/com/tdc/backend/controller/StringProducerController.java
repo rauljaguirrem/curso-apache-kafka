dc129e2c38e5e9e26c3fce3395922bc1222f233f
Raul Aguirre <rjaguirrem@gmail.com>
2026-06-22 22:25:53 -0500

    Curso Apache Kafka - TDC

---
+++ str-producer/src/main/java/com/tdc/backend/controller/StringProducerController.java
@@ -0,0 +1,24 @@
+package com.tdc.backend.controller;
+
+import com.tdc.backend.services.StringProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer")
+public class StringProducerController {
+
+    @Autowired
+    private StringProducerService stringProducerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message){
+        stringProducerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
